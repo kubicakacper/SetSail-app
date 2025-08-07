d54a6459db495301c74c30273d1a10a055e29572
Kacper Kubica <kubicakacper@gmail.com>
2025-08-07 14:59:30 +0200

    Update connCredentials.py

--- load/connCredentials.py
+++ load/connCredentials.py
@@ -1,4 +1,4 @@
 MASTER_USERNAME = 'kakubica'
-PASSWORD = 'Kr0wu1ec'  # obviously, I had to change and hide my password,
+PASSWORD = '<password>'  # obviously, I had to change and hide my password,
 # but the app will work with your relational database
 ENDPOINT = 'setsail-app.cq6kprgd8h6c.eu-central-1.rds.amazonaws.com'
